dde00f5ccb8d58dc35f4658a3104c3b11a181aa6
vagrant <vagrant@localhost.localdomain>
2020-03-26 00:14:07 +0000

    [add]app/scss

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,11 @@ Rails.application.routes.draw do
   #get 'blogs/edit'
   #get 'blogs/update'
   #get 'blogs/destroy'
+  devise_for :users
   resources :users
   #get 'users/index'
   #get 'users/show'
   #get 'users/edit'
   ##get 'users/withdraw'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
