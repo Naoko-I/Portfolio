1caf6dc0f5718a33d20b69055447ce86d3f57adb
vagrant <vagrant@localhost.localdomain>
2020-03-27 09:02:24 +0000

    [add]root/favorites

--- config/routes.rb
+++ config/routes.rb
@@ -7,25 +7,12 @@ Rails.application.routes.draw do
   get 'relationships/followed'
 
   resources :blog_comments
-  #get 'blog_comments/new'
-  #get 'blog_comments/create'
-  #get 'blog_comments/show'
-  #get 'blog_comments/edit'
-  #get 'blog_comments/update'
-  #get 'blog_comments/destroy'
+
   resources :blogs
-  #get 'blogs/new'
-  #get 'blogs/create'
-  #get 'blogs/index'
-  #get 'blogs/show'
-  #get 'blogs/edit'
-  #get 'blogs/update'
-  #get 'blogs/destroy'
+
+  resources :favorites
+
   devise_for :users
   resources :users
-  #get 'users/index'
-  #get 'users/show'
-  #get 'users/edit'
-  ##get 'users/withdraw'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
