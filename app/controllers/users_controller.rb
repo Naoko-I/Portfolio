bf47ac8f72606cd8fa0086f571abdfd8e7bae280
vagrant <vagrant@localhost.localdomain>
2020-04-04 08:58:26 +0000

    [modified]user/show(layout)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @blogs = Blog.all
-    @user = User.find(params[:id])
     @followings = current_user.followings
     @followeds = current_user.followeds
     @blog_comment = BlogComment.new
