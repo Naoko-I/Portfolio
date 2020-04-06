26b6d31e911d0622ca793546b6eb7347ab65b9b9
Naoko-I <naoko.i1024@gmail.com>
2020-04-06 15:34:29 +0900

    [delete]seeds.rb(data)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#ユーザテストデータ
-User.create( name: %?user2?, email: %?user2@pf.com?, password: %?222222?, introduction:%??, profile_image_id:%?? )
-User.create( name: %?user3?, email: %?user3@pf.com?, password: %?333333?, introduction:%??, profile_image_id:%?? )
