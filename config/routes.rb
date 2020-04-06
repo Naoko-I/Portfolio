98a086d1cf19834ba81c39976fd28a7e9186f334
Naoko-I <naoko.i1024@gmail.com>
2020-04-06 21:36:29 +0900

    [delete]migration file

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,22 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about'
 
   resources :blogs do
-   resource :favorites, only: [:index,:create, :destroy]
-   resource :blog_comments, only: [:new, :create, :edit, :update, :destroy]
-   # resource :blog_images, only: [:new, :create, :edit, :update, :destroy]
+    resource :favorites, only: [:index,:create, :destroy]
+    resource :blog_comments, only: [:new, :create, :edit, :update, :destroy]
   end
 
   post '/relationships/:user_id/create' => 'relationships#create', as:'create_relation'
   delete '/relationships/:user_id/destroy' => 'relationships#destroy', as:'destroy_relation'
-  #if文で場合分けできる？<%= if following_userなら「フォローを外す」→「外しますか？メッセージ表示、else「フォローする」%> if followerdなら「followする」→followingへ移動
-  #get '/relationships/show' => 'relationships#show'
 
   # フォローフォロワー一覧ページ作成のため作成
   get '/users/followings' => 'users#followings'
   get '/users/followeds' => 'users#followeds'
-  # 退会確認。最終必要なければ削除
-  get '/users/withdraw' => 'users#withdraw'
 
   devise_for :users
   resources :users, only: [:create, :index, :show, :edit, :update] do
-   get "/favorites" => "favorites#index"
+    get "/favorites" => "favorites#index"
+    # 退会確認。最終必要なければ削除
+    get '/withdraw' => 'users#withdraw'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
