17c6852560d09bf026203f617007310f23222d6d
Naoko-I <naoko.i1024@gmail.com>
2020-04-06 17:43:47 +0900

    [add]uesr.index(layaut)

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+#Run timeエラー解消のため記述
+Refile.secret_key = 'f91aa1d6d0cfe197d41a53fb1fd73d4c519205a2554c05cb515e41fba4bf4d7f9e4c4f19e522b00e98b30bc3b1dcc2214189e4e778756d0baba8a9d8b5892827'
