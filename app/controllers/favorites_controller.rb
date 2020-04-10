ba656eda24b2a8c23c79e535a07a1dfdb8a30575
Naoko-I <naoko.i1024@gmail.com>
2020-04-10 23:41:23 +0900

    [add]favorite.index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,8 @@
 class FavoritesController < ApplicationController
   def index
-  	@user = current_user
-  	@favorite = @user.favorites_blog(params[:blog_id])
-    # @favorites = Blog.find.current_user.favorites(params[:blog_id])
+    @blog = Blog.find(params[:blog_id])
+    @favorites = current_user.favorites.find_by(blog_id: @blog.id, user_id: current_user.id)
+    @blogs = Blog.all
   end
 
   def create
@@ -18,16 +18,16 @@ class FavoritesController < ApplicationController
     favorite.destroy
     redirect_to request.referer
   end
+
+  private
+  	def redirect
+  	  case params[:redirect_id].to_i
+  	  when 0
+  	   redirect_to blogs_path
+  	  when 1
+  	   redirect_to blogs_path(@blog)
+  	   end
+  	end
 end
 
-#応用課題
-# private
-	# def redirect
-	# case params[:redirect_id].to_i
-	# when 0
-	  # redirect_to blogs_path
-	# when 1
-	  # redirect_to blogs_path(@blog)
-	# end
-	# end
-# end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,14 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about'
 
   resources :blogs do
-    resource :favorites, only: [:index,:create, :destroy]
+    resource :favorites, only: [:index, :create, :destroy]
+      get 'favorites/index' => 'favorites#index'
     resource :blog_comments, only: [:new, :create, :edit, :update, :destroy]
   end
-
-  # フォローフォロワー一覧ページ作成のため作成
-  # get '/users/follower' => 'users#follower'
-  # get '/users/followed' => 'users#followed'
 
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update, :destroy] do
     resource :relationships, only: [:create, :destroy]
-    get 'favorites' => 'favorites#index'
     get 'follows' => 'relationships#follower', as: 'follows' # フォローする
     get 'followers' => 'relationships#followed', as: 'unfollows' # フォロー外す
   end
